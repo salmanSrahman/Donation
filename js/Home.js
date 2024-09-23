7747ebef88d12e734e3a638bfb9946f81c399131
Salman-S-Rahman <salmanmern@gmail.com>
2024-09-23 22:26:18 +0600

    main donation js paert end

--- js/Home.js
+++ js/Home.js
@@ -1,1 +1,38 @@
-console.log("Hello World");
+
+
+const noakhaliBtn = document.getElementById("noakhali_btn");
+
+
+noakhaliBtn.addEventListener("click", (e)=>{
+
+    const mainAmount = document.getElementById("main_amount").innerText;
+    const mainAmountNumber = parseFloat(mainAmount);
+
+    let noakhaliAmount = document.getElementById("noakhali_amount").innerText;
+    const noakhaliInput = document.getElementById("noakhali_input").value;
+
+    let noakhaliAmountNumber = parseFloat(noakhaliAmount);
+    let noakhaliInputNumber = parseFloat(noakhaliInput);
+    
+    if(noakhaliInputNumber < 0){
+       alert("Plz add some positive amount;")
+    }
+    else if(isNaN(noakhaliInputNumber)){
+        alert("Plz add some amount;")
+
+    }
+    else{
+        let newAmountNoakhali = noakhaliAmountNumber + noakhaliInputNumber;
+        let newMainAmount = mainAmountNumber - noakhaliInputNumber;
+    
+    
+       
+        document.getElementById("noakhali_amount").innerText = newAmountNoakhali
+        document.getElementById("main_amount").innerText = newMainAmount;
+
+    }
+   
+
+   
+
+})
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {
+        spacing:{
+            container: "1140px"
+        },
+        colors:{
+            primary1: "",
+            secondary1: ""
+        }
+      },
+    },
+    plugins: [],
+  }
