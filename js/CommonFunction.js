27ec8980b1f1b0b3214cb1d02805bf39245a9266
Salman-S-Rahman <salmanmern@gmail.com>
2024-09-24 21:10:35 +0600

    all donation part end

--- js/CommonFunction.js
+++ js/CommonFunction.js
@@ -13,3 +13,19 @@ const amountInputValue = (id) =>{
     return amountNumber;
 
 }
+
+
+function openModal(id) {
+    document.getElementById(id).classList.add("modal-show");
+    document.getElementById(id).classList.remove("hidden", "modal-hide");
+
+}
+
+function modalClose(id){
+    document.getElementById(id).classList.add("hidden","modal-hide");
+    document.getElementById(id).classList.remove("modal-show");
+}
+
+
+
+
--- js/Home.js
+++ js/Home.js
@@ -1,25 +1,37 @@
 const noakhaliBtn = document.getElementById("noakhali_btn");
+const feniBtn = document.getElementById("feni_btn");
+const quotaBtn = document.getElementById("quota_btn");
 
-const modal = document.getElementById("modal");
-const modalCloseBtn = document.getElementById("modal_close");
+const donationPart = document.getElementById("donation_part");
+const transactionPart = document.getElementById("transaction_part");
 
-modalCloseBtn.addEventListener("click", (e)=>{
-    closeModal();
-})
+const donationBtn = document.getElementById("donation_btn");
+const historyBtn = document.getElementById("history_btn");
 
+donationBtn.addEventListener("click", ()=>{
+    donationPart.classList.add("hidden");
+    transactionPart.classList.add("hidden");
 
-function closeModal(){
-    modal.classList.add("hidden","modal-hide");
-    modal.classList.remove("modal-show");
+    // button design toggle
+    historyBtn.classList.remove("bg-[#B4F461]", "border-[#b4F461]");
+    historyBtn.classList.add("border-[#1111]/30");
     
-}
+    // donation part shown
+    donationPart.classList.remove("hidden");
+    donationBtn.classList.add("bg-[#B4F461]", "border-[#b4F461]");
+})
+historyBtn.addEventListener("click", ()=>{
+    donationPart.classList.add("hidden");
+    transactionPart.classList.add("hidden");
 
-
-function openModal() {
-    modal.classList.add("modal-show");
-    modal.classList.remove("hidden", "modal-hide");
-
-}
+    // button design toggle
+    donationBtn.classList.remove("bg-[#B4F461]", "border-[#b4F461]");
+    donationBtn.classList.add("border-[#1111]/30");
+    
+    // transaction part shown
+    transactionPart.classList.remove("hidden");
+    historyBtn.classList.add("bg-[#B4F461]", "border-[#b4F461]");
+})
 
 
 
@@ -49,12 +61,97 @@ noakhaliBtn.addEventListener("click", (e)=>{
 
     
         document.getElementById("noakhali_input").value = "";
-        openModal();
+        openModal("noakhali_modal");
 
     }
 
 
 })
+
+
+
+
+
+feniBtn.addEventListener("click", (e)=>{
+
+    const mainAmountNumber = amountText("main_amount")
+    const feniAmountNumber = amountText("feni_amount");
+    const feniInputNumber = amountInputValue("feni_input");
+    
+    
+    if(feniInputNumber < 0){
+       alert("Plz add some positive amount;")
+    }
+    else if(isNaN(feniInputNumber)){
+        alert("Plz add some amount;")
+        
+    }
+    else if(mainAmountNumber == 0 || feniInputNumber > mainAmountNumber){
+        alert("You dont have enough balance.")
+    }
+    else{
+        let newAmountFeni = feniAmountNumber + feniInputNumber;
+        let newMainAmount = mainAmountNumber - feniInputNumber;
+
+        document.getElementById("feni_amount").innerText = newAmountFeni
+        document.getElementById("main_amount").innerText = newMainAmount;
+
+    
+        document.getElementById("feni_input").value = "";
+        openModal("feni_modal");
+
+    }
+
+
+})
+
+quotaBtn.addEventListener("click", (e)=>{
+
+    const mainAmountNumber = amountText("main_amount")
+    const quotaAmountNumber = amountText("quota_amount");
+    const quotaInputNumber = amountInputValue("quota_input");
+    
+    
+    if(quotaInputNumber < 0){
+       alert("Plz add some positive amount;")
+    }
+    else if(isNaN(quotaInputNumber)){
+        alert("Plz add some amount;")
+        
+    }
+    else if(mainAmountNumber == 0 || quotaInputNumber > mainAmountNumber){
+        alert("You dont have enough balance.")
+    }
+    else{
+        let newAmountQuota = quotaAmountNumber + quotaInputNumber;
+        let newMainAmount = mainAmountNumber - quotaInputNumber;
+
+        document.getElementById("quota_amount").innerText = newAmountQuota
+        document.getElementById("main_amount").innerText = newMainAmount;
+
+    
+        document.getElementById("quota_input").value = "";
+        openModal("quota_modal");
+
+    }
+
+
+})
+
+document.getElementById("noakhali_modal_close").addEventListener("click", (e)=>{
+    modalClose("noakhali_modal")
+
+});
+
+document.getElementById("feni_modal_close").addEventListener("click", (e)=>{
+    modalClose("feni_modal")
+
+});
+
+document.getElementById("quota_modal_close").addEventListener("click", (e)=>{
+    modalClose("quota_modal")
+
+});
 
 
 
